4f8c43530571625e1fa4f9cfad34783ea5e45d47
Pierre Romera <hello@pirhoo.com>
2026-01-24 12:42:55 +0000

    feat: update mixins and utils for vue 3 compatibility
    Co-Authored-By: @agent-Committer

--- src/mixins/colors.js
+++ src/mixins/colors.js
@@ -1,47 +1,45 @@
-import { bisector } from 'd3';
-import * as chroma from 'chroma-js';
+import { bisector } from 'd3'
+import * as chroma from 'chroma-js'
 import {
-  range, first, last, reduce, map, shuffle, identity,
-} from 'lodash';
-import gradients from '@/assets/json/gradients.json';
-/* ignore eslint start */
-import scss from '!!dart-sass-variable-loader!@/utils/_variables.scss';
-/* ignore eslint end */
+  range, first, last, reduce, map, shuffle, identity
+} from 'lodash'
+import gradients from '@/assets/json/gradients.json'
+import scss from '@/utils/variables.js'
 
 // Only create the slice once
-export const gradientsSlice = shuffle(gradients).slice(0, 16);
+export const gradientsSlice = shuffle(gradients).slice(0, 16)
 
 // Export colors from here to avoid ignoring eslint again...
-export { scss };
+export { scss }
 
 export default {
   computed: {
     colorScaleText() {
-      return ratio => chroma.mix(this.colorScalePrimary(ratio), scss.bodyColor, 0.8);
+      return ratio => chroma.mix(this.colorScalePrimary(ratio), scss.bodyColor, 0.8)
     },
     colorScalePrimary() {
-      const scale = chroma.scale(map(this.gradients, first));
-      return ratio => scale(ratio).hex();
+      const scale = chroma.scale(map(this.gradients, first))
+      return ratio => scale(ratio).hex()
     },
     colorScaleSecondary() {
-      const scale = chroma.scale(map(this.gradients, last));
-      return ratio => scale(ratio).hex();
+      const scale = chroma.scale(map(this.gradients, last))
+      return ratio => scale(ratio).hex()
     },
     gradients() {
-      return reduce(gradientsSlice, (all, gradient) => all.concat([gradient.colors]), []);
+      return reduce(gradientsSlice, (all, gradient) => all.concat([gradient.colors]), [])
     },
     domains() {
-      const tick = 1 / this.gradients.length;
-      return range(0, 1 + tick, tick);
+      const tick = 1 / this.gradients.length
+      return range(0, 1 + tick, tick)
     },
     scss() {
-      return scss;
-    },
+      return scss
+    }
   },
   methods: {
     noramlizedRatio(ratio) {
-      const index = bisector(identity).left(this.domains, ratio);
-      return this.domains[index];
-    },
-  },
-};
+      const index = bisector(identity).left(this.domains, ratio)
+      return this.domains[index]
+    }
+  }
+}
