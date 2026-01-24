4e73213ae6f24971677bd4c233a98dd2c239b996
Pierre Romera <hello@pirhoo.com>
2026-01-24 13:44:11 +0000

    feat: remove fontawesome registration and components
    Co-Authored-By: @agent-Committer

--- src/main.js
+++ src/main.js
@@ -3,12 +3,10 @@ import VueLazyload from 'vue3-lazyload'
 import VueMasonryCSS from 'vue-masonry-css'
 
 import App from './components/App.vue'
-import FontAwesomeIcon from './components/FontAwesomeIcon'
 
 const app = createApp(App)
 
 app.use(VueLazyload, { lazyComponent: true, preLoad: 1 })
 app.use(VueMasonryCSS)
-app.component('Fa', FontAwesomeIcon)
 
 app.mount('#app')
