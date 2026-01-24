1fec96ab2d250175ccdd6aa8bdde6345b58b7d2d
Pierre Romera <hello@pirhoo.com>
2026-01-24 12:42:49 +0000

    feat: migrate to vue 3 createApp and mitt event bus
    Co-Authored-By: @agent-Committer

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
-import Vue from 'vue';
-import VueLazyload from 'vue-lazyload';
-import VueMasonry from 'vue-masonry-css';
+import { createApp } from 'vue'
+import VueLazyload from 'vue3-lazyload'
+import VueMasonryCSS from 'vue-masonry-css'
 
-import App from './components/App.vue';
-import FontAwesomeIcon from './components/FontAwesomeIcon';
+import App from './components/App.vue'
+import FontAwesomeIcon from './components/FontAwesomeIcon'
 
-Vue.use(VueLazyload, { lazyComponent: true, preLoad: 1 });
-Vue.use(VueMasonry);
-Vue.component('fa', FontAwesomeIcon);
-Vue.config.productionTip = false;
+const app = createApp(App)
 
-new Vue({ render: h => h(App) }).$mount('#app');
+app.use(VueLazyload, { lazyComponent: true, preLoad: 1 })
+app.use(VueMasonryCSS)
+app.component('Fa', FontAwesomeIcon)
+
+app.mount('#app')
