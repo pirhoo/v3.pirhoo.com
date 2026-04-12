87ce8e7cfe9a4c60698b92cc9b43af8b6df34374
pirhoo <hello@pirhoo.com>
2026-04-12 17:08:33 +0000

    feat(About): replace label TextReveal with plain span and add line-by-line content reveal

--- src/components/Section/Introduction/About.vue
+++ src/components/Section/Introduction/About.vue
@@ -1,44 +1,37 @@
 <template>
-  <div class="introduction-about section-border" :style="{ transform: `translateY(${parallaxOffset}px)` }">
+  <div ref="aboutRef" class="introduction-about section-border" :style="{ transform: `translateY(${parallaxOffset}px)` }">
     <layout-section-grid>
       <template #label>
-        <app-text-reveal
-          text="About"
-          tag="span"
-          class="text-label text-label--section"
-          :delay="100"
-        />
+        <span class="text-label text-label--section">About</span>
       </template>
       <template #content>
-        <div class="introduction-about__content">
-          <p
-            class="text-body"
-          >
-            I'm CTO at the <a href="http://icij.org">International Consortium
-              of Investigative Journalists (ICIJ)</a> where I build
-            interactive stories, data driven investigations and tools.
-          </p>
-          <p class="text-body">
-            I used to be CTO at <a href="http://jplusplus.org">Journalism++</a>,
-            an agency that I co-founded after having been lead developer
-            at <a href="http://owni.pirhoo.com">OWNI</a>. Between 2012 and 2019, I was an Associate Professor
-            at Sciences Po and many other journalism schools, where I
-            taught programming and interactive storytelling.
-          </p>
-        </div>
+        <p class="introduction-about__paragraph text-body">
+          I'm CTO at the <a href="http://icij.org">International Consortium
+            of Investigative Journalists (ICIJ)</a> where I build
+          interactive stories, data driven investigations and tools.
+        </p>
+        <p class="introduction-about__paragraph text-body">
+          I used to be CTO at <a href="http://jplusplus.org">Journalism++</a>,
+          an agency that I co-founded after having been lead developer
+          at <a href="http://owni.pirhoo.com">OWNI</a>. Between 2012 and 2019, I was an Associate Professor
+          at Sciences Po and many other journalism schools, where I
+          taught programming and interactive storytelling.
+        </p>
       </template>
     </layout-section-grid>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import { useParallax } from '@/composables/useParallax'
+import { useContentReveal } from '@/composables/useContentReveal'
 import LayoutSectionGrid from '@/components/Layout/SectionGrid.vue'
-import AppTextReveal from '@/components/App/TextReveal.vue'
 
 const aboutRef = ref(null)
+const contentRef = computed(() => aboutRef.value?.querySelector('.section-grid__content'))
 const { offset: parallaxOffset } = useParallax(aboutRef, { speed: 0.1 })
+useContentReveal(contentRef)
 </script>
 
 <style lang="scss">
@@ -51,15 +44,12 @@ const { offset: parallaxOffset } = useParallax(aboutRef, { speed: 0.1 })
     padding: $space-15 0;
   }
 
-  &__content {
+  &__paragraph {
     max-width: 600px;
+    margin: 0 0 $space-5;
 
-    p {
-      margin: 0 0 $space-5;
-
-      &:last-child {
-        margin-bottom: 0;
-      }
+    &:last-child {
+      margin-bottom: 0;
     }
   }
 }
